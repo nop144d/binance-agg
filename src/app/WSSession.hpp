c4a58681c10946bd6691dac2bf2cccce3c76abd6
SURFACE\nop14 <nop144d@gmail.com>
2026-09-18 00:31:11 +0400

    Added minimal WebSocket client
    Used boost's official example to implement WS session

---
+++ src/app/WSSession.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+// based on the Boost.Beast WebSocket SSL client example:
+// https://github.com/boostorg/beast/blob/develop/example/websocket/client/async-ssl/websocket_client_async_ssl.cpp
+
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/beast/ssl.hpp>
+#include <boost/beast/websocket/ssl.hpp>
+#include <boost/asio/ssl.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/strand.hpp>
+#include <memory>
+#include <string>
+
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace websocket = beast::websocket;
+namespace net = boost::asio;
+namespace ssl = boost::asio::ssl;
+using tcp = boost::asio::ip::tcp;
+
+namespace binagg
+{
+
+    class WSSession : public std::enable_shared_from_this<WSSession>
+    {
+    public:
+        explicit WSSession(net::io_context& ioc, ssl::context& ctx)
+            : resolver_(net::make_strand(ioc))
+            , ws_(net::make_strand(ioc), ctx)
+        {
+        }
+
+        void run(std::string host, std::string port, std::string target);
+
+    private:
+        void on_resolve(beast::error_code ec, tcp::resolver::results_type results);
+        void on_connect(beast::error_code ec, tcp::resolver::results_type::endpoint_type ep);
+        void on_ssl_handshake(beast::error_code ec);
+        void on_handshake(beast::error_code ec);
+        void do_read();
+        void on_read(beast::error_code ec, std::size_t bytes_transferred);
+        void on_close(beast::error_code ec);
+
+        tcp::resolver resolver_;
+        websocket::stream<beast::ssl_stream<beast::tcp_stream>> ws_;
+        beast::flat_buffer buffer_;
+        std::string host_;
+        std::string target_;
+    };
+
+    void fail(beast::error_code ec, char const* what);
+
+} // namespace binagg
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#include "app/WSSession.hpp"
+
+using namespace binagg;
+
 int main(int argc, char* argv[]) {
 	if (argc != 2) {
 		std::cerr << "Usage: " << argv[0] << " <config file>" << std::endl;
@@ -8,5 +12,19 @@ int main(int argc, char* argv[]) {
 	else {
 		std::cout << "Config file: " << argv[1] << std::endl;
 	}
+
+    ssl::context ctx{ ssl::context::tlsv13_client };
+	ctx.set_verify_mode(ssl::verify_none); // Disable certificate verification for testing purposes
+    ctx.set_default_verify_paths();
+
+    std::string const host{"stream.binance.com"};
+    std::string const port{"9443"};
+    std::string const target{"/stream?streams=btcusdt@trade"};
+
+    net::io_context ioc;
+    std::make_shared<WSSession>(ioc, ctx)->run(std::move(host), std::move(port), std::move(target));
+
+    ioc.run();
+
     return 0;
 }
